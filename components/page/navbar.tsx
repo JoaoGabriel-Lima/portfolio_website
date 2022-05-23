fec71d4759aab4d491ded8019125aa756e92f59d
João Lima <jg.limamarinho202@gmail.com>
2022-05-22 22:23:18 -0300

    🐛 Fix: Another Scroll Bug

--- components/page/navbar.tsx
+++ components/page/navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = (props: NavbarProps) => {
     <nav className=" w-full p-2 h-14 bg-[#ffffff40] dark:bg-[#20202380] z-[1] fixed backdrop-blur-md flex justify-center items-center">
       <section className="max-w-[730px] w-full h-[40px] flex justify-between items-center">
         <div id="left" className="flex sm:ml-0 ml-3">
-          <NextLink href="/">
+          <NextLink href="/" passHref scroll={false}>
             <a>
               <span className="text-black text-lg font-bold flex items-center">
                 <div className="bg-[#e28b40] dark:bg-[#eb9d5a] rounded-full w-5 h-5 mr-2"></div>
